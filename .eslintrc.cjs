97f9b88a4c144e0d3b3e673872d5afd01300cf4e
Alejandro Ivan Lizarraga Rojas <alejandroivanliza@gmail.com>
2023-04-23 12:47:38 -0700

    Configuracion de ambiente de pruebas

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,10 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+    "jest": true,
+    node: true
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
@@ -7,7 +12,11 @@ module.exports = {
     'plugin:react-hooks/recommended',
   ],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
+  settings: {
+    react: {
+      version: '18.2'
+    }
+  },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
