6d2d0c7c9ada345faed469a8a8cf5c61ec15bb02
Alejandro Ivan Lizarraga Rojas <alejandroivanliza@gmail.com>
2023-04-22 22:13:38 -0700

    Added styles for button

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 import { useState } from "react";
 
 export const AddCategory = ({ onNewCategory }) => {
-	const [inputValue, setInputValue] = useState("One Punch");
+	const [inputValue, setInputValue] = useState("");
 
 	const onInputChange = ({ target }) => setInputValue(target.value);
 
@@ -20,7 +20,9 @@ export const AddCategory = ({ onNewCategory }) => {
 	return (
 		<form onSubmit={onSubmit}>
 			<input type="text" placeholder="Buscar Gifs" value={inputValue} onChange={onInputChange} />
-			<button type="submit">Add Category</button>
+			<button type="submit" className="btn-search">
+				Search GIFs
+			</button>
 		</form>
 	);
 };
